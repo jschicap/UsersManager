a696474f3965b25ee5a7b6ec409a0af43c82f2f1
jschicap <jschicap@gmail.com>
2025-03-23 09:57:52 -0500

    first create
    service working

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,33 @@
+// src/users/users.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../interfaces/user.interface';
+import { CreateUserDto } from '../dto/user.dto';
+import { UpdateUserDto } from '../dto/user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<User | null> {
+    return this.userModel.findById(id).exec();
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
+  }
+}
